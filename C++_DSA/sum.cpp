71350d4f2c11a74954dce37e8810c7063cca2330
Surajsavy <93027224+Surajsavy@users.noreply.github.com>
2023-10-02 14:38:21 +0000

    more code added

--- C++_DSA/sum.cpp
+++ C++_DSA/sum.cpp
@@ -1,11 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int a,b;
-    cout<<" Enter First Number :"<<endl;
-    cin>>a;
-    cout<<"Enter Second Number :"<<endl;
-    cin>>b;
-    cout<<"The sum of two Number is :"<<a+b<<endl;
+int main()
+{
+    int a, b;
+    cout << " Enter First Number :" << endl;
+    cin >> a;
+    cout << "Enter Second Number :" << endl;
+    cin >> b;
+    cout << "The sum of two Number is :" << a + b << endl;
     return 0;
+    // TYpecasting
+    int a = 11212;
+    char c = 'd';
+    c = a;
+    cout << c << endl;
 }
