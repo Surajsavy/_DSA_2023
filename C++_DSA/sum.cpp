99978d346b2423c9b36076022955679b990aaf0a
Surajsavy <93027224+Surajsavy@users.noreply.github.com>
2023-10-02 08:26:14 +0000

    code added in codespace

---
+++ C++_DSA/sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<" Enter First Number :"<<endl;
+    cin>>a;
+    cout<<"Enter Second Number :"<<endl;
+    cin>>b;
+    cout<<"The sum of two Number is :"<<a+b<<endl;
+    return 0;
+}
