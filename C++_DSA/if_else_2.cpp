6c1a07c28e303d50759ddc5c86e34318e6852d98
Surajsavy <93027224+Surajsavy@users.noreply.github.com>
2023-10-02 17:04:26 +0000

    code on conditionals

---
+++ C++_DSA/if_else_2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number;
+    cout<<"Enter the number :"<<endl;
+    cin>>number;
+    if(number>0){
+        cout<<"Number is positive"<<endl;
+    }
+    else if(number==0){
+        cout<<"0";
+    }
+    else{
+        cout<<"Number is Negetive"<<endl;
+        }
+}
