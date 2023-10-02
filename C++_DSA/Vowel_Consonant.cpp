66d6b850285ec1a986dc8173dc3a9da554c465e8
Surajsavy <93027224+Surajsavy@users.noreply.github.com>
2023-10-02 17:29:30 +0000

    more on conditionals

---
+++ C++_DSA/Vowel_Consonant.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout << "Enter a Character :" << endl;
+    cin >> ch;
+    if (ch == 'a')
+    {
+        cout << "The Character is a vowel" << endl;
+    }
+    else if (ch == 'e')
+    {
+        cout << "The Character is a vowel" << endl;
+    }
+    else if (ch == 'i')
+    {
+        cout << "The Character is a vowel" << endl;
+    }
+    else if (ch == 'o')
+    {
+        cout << "The Character is a vowel" << endl;
+    }
+    else if (ch == 'u')
+    {
+        cout << "The Character is a vowel" << endl;
+    }
+    else
+    {
+        cout << "The character is a Consonant" << endl;
+    }
+}
