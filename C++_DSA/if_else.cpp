be7093e27ecb0fbc6fa74e0beb6aff6d0646033b
Surajsavy <93027224+Surajsavy@users.noreply.github.com>
2023-10-02 15:30:45 +0000

    more code added

---
+++ C++_DSA/if_else.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int a,b;
+ cout<<"Enter your first number:"<<endl;
+ cin>>a;
+ cout<<"Enter your Second Number:"<<endl;
+ cin>>b;
+ if(a>b){
+    cout<<"yes"<<endl;
+ }
+ else{
+    cout<<"No"<<endl;
+ }
+ 
+    return 0;
+}
