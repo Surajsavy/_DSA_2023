9c6602fd3f4a9c89b758f6e9c70958a8251e2d9b
Surajsavy <93027224+Surajsavy@users.noreply.github.com>
2023-10-02 15:34:45 +0000

    more code added

---
+++ C++_DSA/Even_Odd.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number;
+    cout<<"Enter your  Number :"<<endl;
+    cin>>number;
+    if(number%2==0){
+        cout<<"Even"<<endl;
+    }
+    else{
+        cout<<"Odd"<<endl;
+    }
+}
